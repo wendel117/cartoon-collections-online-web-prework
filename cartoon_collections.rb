5afab7fd16a1ded3f943902e61b2a00266354e7c
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-12 20:02:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,14 +15,14 @@ def summon_captain_planet(array)
   nu_array
 end
 
-def long_planeteer_calls(array) 
-  i = 0 
+def long_planeteer_calls(array)
+  i = 0
   if  array.any? {|i| i.length > 4}
     return true
-  else 
+  else
     return false
   i = i + 1
-  end 
+  end
 end
 
 #def find_the_cheese# code an argument here
