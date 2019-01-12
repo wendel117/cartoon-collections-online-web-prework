09ea265c4ab696a7e76c69f9a8d36a124f8ac529
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-12 20:02:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,11 @@ def roll_call_dwarves(array)
 end
 def summon_captain_planet(array)
   nu_array = []
-  i = 0 
-  while i < array.length 
+  i = 0
+  while i < array.length
     nu_array << array[i].capitalize + "!"
-    i += 1 
-  end 
+    i += 1
+  end
   nu_array
 end
 
